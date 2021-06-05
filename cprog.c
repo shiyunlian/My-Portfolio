87295574582cefa94348d8fc1bb39a5140fb4324
shiyunlian <shiyunlian00@gmail.com>
2021-06-05 10:44:37 -0700

    remove old codes

--- cprog.c
+++
@@ -1,5 +0,0 @@
-#include<stdio.h>
-int main(){
-printf("this is c program");
-return 0;
-}
--- javaprog.java
+++
@@ -1,7 +0,0 @@
-class Javaprog{
-
-public static void main(String[] args){
-
-System.out.println("hello");
-}
-}

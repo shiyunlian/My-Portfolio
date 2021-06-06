861c87539685dd3f572743239c42bb2e45e29c9c
shiyunlian <shiyunlian00@gmail.com>
2021-06-05 21:24:30 -0700

    test

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-caches-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("/Users/shiyunlian/Projects/My-Portfolio/.cache/caches/gatsby-plugin-offline/app-shell.js"))),
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/shiyunlian/Projects/My-Portfolio/.cache/dev-404-page.js"))),
   "component---node-modules-lekoarts-gatsby-theme-cara-src-templates-cara-tsx": hot(preferDefault(require("/Users/shiyunlian/Projects/My-Portfolio/node_modules/@lekoarts/gatsby-theme-cara/src/templates/cara.tsx")))
 }
 
